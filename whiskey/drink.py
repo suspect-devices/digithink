27199048c53b3a4e6c86f4ce758c41cd36d43f48
D Delmar Davis <don@suspectdevices.com>
2024-06-25 21:54:40 -0700

    this should work

--- whiskey/drink.py
+++ whiskey/drink.py
@@ -1,9 +1,4 @@
-# cd /var/www/digithink
-# git pull && mkdocs build -d stage
-# rm -rf osite&&mv site osite&&mv stage site
-# git pull && mkdocs build -d stage
-# rm -rf osite&&mv site osite&&mv stage site
-#
+
 from flask import Flask
 from markupsafe import escape
 import subprocess
@@ -12,8 +7,7 @@ app = Flask(__name__)
 
 @app.route("/whiskey/<style>",methods = ['POST', 'GET'])
 def whiskey(style):
-    ret=subprocess.call(['git', 'pull'], cwd="/var/www/digithink")
-    ret=subprocess.call(['mkdocs', 'build'], cwd="/var/www/digithink")
+#    ret=subprocess.call(['mkdocs', 'build'], cwd="/var/www/digithink")
     return f"One Whiskey, {escape(style)}!"
 
 if __name__ == "__main__":
---
+++ whiskey/pullandbuild.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+cd /var/www/digithink
+git pull && mkdocs build -d stage
+rm -rf osite&&mv site osite&&mv stage site
+git pull && mkdocs build -d stage
+rm -rf osite&&mv site osite&&mv stage site
