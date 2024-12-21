961659d0b504f2cb8ae916accd818af1815297ce
feurig <don@suspectdevices.com>
2024-12-21 11:18:23 +0100

    test this

--- whiskey/pullandbuild.sh
+++ whiskey/pullandbuild.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
-cd /var/www/digithink
-git pull && mkdocs build -d stage
-cp -r overrides stage/
-rm -rf osite
-mv site osite
-mv stage site
-cd /var/www/digithink/whiskey/bartender
-git pull && mkdocs build
-cp -rpvf site/. .
+{
+    cd /var/www/digithink
+    git pull && mkdocs build -d stage
+    cp -r overrides stage/
+    rm -rf osite
+    mv site osite
+    mv stage site
+    cd /var/www/digithink/whiskey/bartender
+    git pull && mkdocs build
+    cp -rpvf site/. .
+}| tee logs/pullandbuild.out
