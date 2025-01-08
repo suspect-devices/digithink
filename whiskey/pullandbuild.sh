3465c68e4e6cd62f5b603b0803f15d9fd71f8bad
D Delmar Davis <don@suspectdevices.com>
2025-01-07 22:11:29 -0800

    shuffle

--- whiskey/pullandbuild.sh
+++ whiskey/pullandbuild.sh
@@ -1,14 +1,13 @@
 #!/bin/sh
 # Lots of hard coded foo here. This is very digithink.com specific.
 {   echo "====================== Start Build `date`==================="
-    cd /var/www/digithink
-    git pull && mkdocs build -d stage
-    cp -r overrides stage/
-    rm -rf osite
-    mv site osite
-    mv stage site
-    cd /var/www/digithink/whiskey/bartender
+    cd /var/www/digithink/repo
+    git pull && mkdocs build 
+    rm -rf ../osite
+    mv ../site ../osite
+    mv site
+    cd /var/www/digithink/repo/whiskey/bartender
     git pull && mkdocs build
-    cp -rpvf site/. .
+    cp -rpvf site/. /var/www/digithink/whiskey/bartender/
     echo "=========================== Done: `date` ===================="
 }| tee logs/pullandbuild.out
